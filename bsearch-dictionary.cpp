46c381fe0fea78806e60b8ade3699def0f880d5c
abhiga <agaurav@purdue.edu>
2015-07-20 18:54:26 -0400

    Commit search engine

--- bsearch-dictionary.cpp
+++ bsearch-dictionary.cpp
@@ -31,7 +31,21 @@ BinarySearchDictionary::findRecord( KeyType key)
 	// Use binary search
 
 	// Add your implementation here
-
+	int low = 0;
+	int high = currentNumber - 1;
+	while (high > low){
+		int mid = (high + low) / 2;
+		int result = strcmp(key, array[mid].key);
+		if (result == 0){
+			return array[mid].data;
+		}
+		else if (result < 0){
+			high = mid - 1;
+		}
+		else{
+			low = mid + 1;
+		}
+	}
 	return NULL;
 }
 
