28abc2f598bdc5ce0f60a6021b10f12fa8bbee73
abhiga <agaurav@purdue.edu>
2015-07-22 19:09:42 -0400

    Commit search engine

--- search-engine.cpp
+++ search-engine.cpp
@@ -113,15 +113,15 @@ SearchEngine::SearchEngine( int port, DictionaryType dictionaryType):
 		char *word;
 		while (fgets(temp, 1000, fd)) {
 			if (strcmp(temp, "\n")) {
-				elem = strtok(temp, " \n");
+				elem = strtok(temp, " ");
 				word = strdup(elem);
-				elem = strtok(NULL, " \n");
+				//elem = strtok(NULL, " \n");
 				printf("%s\n", word);
-				while (elem!=NULL) {
+				/*while (elem!=NULL) {
 					index = atoi(elem);
 					printf("%d\n", index);
-					elem = strtok(NULL, " \n");
-				}
+					elem = strtok(NULL, " \n");*/
+				//}
 			} 
 		}
 	}		
