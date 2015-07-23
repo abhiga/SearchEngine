9e63ad50f2d77c1736de030340bafd156d8ec382
abhiga <agaurav@purdue.edu>
2015-07-23 09:55:40 -0400

    Commit search engine

--- avl-dictionary.cpp
+++ avl-dictionary.cpp
@@ -113,140 +113,138 @@ AVLDictionary::restructure(AVLNode * n) {
         // See class notes
 
 	// Add your implementation here
-	AVLNode *z = n->parent;
+	AVLNode *z = n -> parent;
 	
     while(z != NULL)
 	{
         int heightleft = 0;
-        if(z->left != NULL)
-            heightleft = z->left->height;
+        if(z -> left != NULL)
+            heightleft = z -> left -> height;
 		
         int heightright = 0;
-        if(z->right != NULL)
-            heightright = z->right->height;
+        if(z -> right != NULL)
+            heightright = z -> right-> height;
 		int heightdiff = heightleft - heightright;
         if(heightdiff < 0)
             heightdiff = -heightdiff;
 		if(heightleft > heightright)
-            z->height = 1+heightleft;
+            z -> height = 1 + heightleft;
         else
-            z->height = 1+heightright;
+            z -> height = 1 + heightright;
 
         if(heightdiff <= 1) {
-            z = z->parent;
+            z = z -> parent;
             continue;
         }
 
         AVLNode *y = NULL;
         int maxheight = 0;
-        if(z->left != NULL) {
-            y = z->left;
-            maxheight = y->height;
+        if(z -> left != NULL) {
+            y = z -> left;
+            maxheight = y -> height;
         }
-        if(z->right != NULL && z->right->height > maxheight) {
-            y = z->right;
-            maxheight = y->height;
+        if(z -> right != NULL && z -> right -> height > maxheight) {
+            y = z -> right;
+            maxheight = y -> height;
         }
 		AVLNode *x = NULL;
         maxheight = 0;
-        if(y->left != NULL){
-            x = y->left;
-            maxheight = x->height;
+        if(y -> left != NULL){
+            x = y -> left;
+            maxheight = x -> height;
         }
-        if(y->right != NULL && y->right->height > maxheight) {
-            x = y->right;
-            maxheight = x->height;
+        if(y -> right != NULL && y -> right -> height > maxheight) {
+            x = y -> right;
+            maxheight = x -> height;
         }
         assert(x != NULL);
 
         AVLNode * a, * b, * c, * t0, * t1, * t2, * t3;
-        if(z->right == y) {
-            if(y->right == x) {
+        if(z -> right == y) {
+            if(y -> right == x) {
                 a = z;
                 b = y;
                 c = x;
-                t0 = z->left;
-                t1 = y->left;
-                t2 = x->left;
-                t3 = x->right;
+                t0 = z -> left;
+                t1 = y -> left;
+                t2 = x -> left;
+                t3 = x -> right;
             }
             else {
                 a = z;
                 b = x;
                 c = y;
-                t0 = z->left;
-                t1 = x->left;
-                t2 = y->left;
-                t3 = y->right;
+                t0 = z -> left;
+                t1 = x -> left;
+                t2 = y -> left;
+                t3 = y -> right;
             }
         }
         else {
-            if(y->left == x){
+            if(y -> left == x){
                 a = x;
                 b = y;
                 c = z;
-                t0 = x->left;
-                t1 = x->right;
-                t2 = y->right;
-                t3 = z->right;
+                t0 = x -> left;
+                t1 = x -> right;
+                t2 = y -> right;
+                t3 = z -> right;
             }
             else{
                 a = y;
                 b = x;
                 c = z;
-                t0 = y->left;
-                t1 = x->left;
-                t2 = x->right;
-                t3 = z->right;
+                t0 = y -> left;
+                t1 = x -> left;
+                t2 = x -> right;
+                t3 = z -> right;
             }
         }
 
-        AVLNode *p;
-		p = z->parent;
-        if(p == NULL){
+        AVLNode *p = z->parent;
+        if(p == NULL)
             root = b;
-        }
         else{
-            if(b->left == z)
-                p->left = b;
+            if(b -> left == z)
+                p -> left = b;
             else
-                p->right = b;
+                p -> right = b;
         }
-		b->parent = p;
-        b->left = a;
-        b->right = c;
-        a->parent = b;
-        a->left = t0;
-        a->right = t1;
-        c->parent = b;
-        c->left = t2;
-        c->right = t3;
+		b -> parent = p;
+        b -> left = a;
+        b -> right = c;
+        a -> parent = b;
+        a -> left = t0;
+        a -> right = t1;
+        c -> parent = b;
+        c -> left = t2;
+        c -> right = t3;
 		if(t0 != NULL)
-            t0->parent = a;
+            t0 -> parent = a;
         if(t1 != NULL)
-            t1->parent = a;
+            t1 -> parent = a;
         if(t2 != NULL)
-            t2->parent = c;
+            t2 -> parent = c;
         if(t3 != NULL)
-            t3->parent = c;
+            t3 -> parent = c;
 		maxheight = 0;
-        if(a->left != NULL)
-            maxheight = a->left->height;
-        if(a->right != NULL && a->right->height > maxheight)
-            maxheight = a->right->height;
-        a->height = 1+maxheight;
+        if(a -> left != NULL)
+            maxheight = a -> left -> height;
+        if(a -> right != NULL && a -> right -> height > maxheight)
+            maxheight = a -> right -> height;
+        a -> height = 1 + maxheight;
 		maxheight = 0;
-        if(c->left != NULL)
-            maxheight = c->left->height;
-        if(c->right != NULL && c->right->height > maxheight)
-            maxheight = c->right->height;
-        c->height = 1+maxheight;
+        if(c -> left != NULL)
+            maxheight = c -> left -> height;
+        if(c -> right != NULL && c -> right -> height > maxheight)
+            maxheight = c -> right -> height;
+        c -> height = 1 + maxheight;
 		maxheight = 0;
-        if(b->left != NULL)
-            maxheight = b->left->height;
-        if(b->right != NULL && b->right->height > maxheight)
-            maxheight = b->right->height;
-        b->height = 1+maxheight;
+        if(b -> left != NULL)
+            maxheight = b -> left -> height;
+        if(b -> right != NULL && b -> right -> height > maxheight)
+            maxheight = b -> right -> height;
+        b -> height = 1+maxheight;
 		z = p;
     }
 }
@@ -261,7 +259,7 @@ AVLDictionary::findRecord( KeyType key)
 	while (e != NULL) {
 		if (strcmp(e -> key, key) == 0)
 			return e -> data;
-		else if (strcmp(e->key, key) < 0)
+		else if (strcmp(e -> key, key) < 0)
 			e = e -> right;
 		else
 			e = e -> left;
